4c1af2571173c4f1981c8847b12c8603177569c8
Romain Deneau <rdeneau@yahoo.fr>
2020-12-01 16:35:18 +0100

    add console and test setup

---
+++ src/console/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+module Ok =
+    let a = 1
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ src/testing/Main.fs
@@ -0,0 +1,6 @@
+﻿module testing
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
